9fd8254fae38d184375e5db15cb644cb25d53a1e
AndreyLukashenko <LukashenkoAndrey@gmail.com>
2016-11-27 19:42:06 +0300

    Add output config
    Add hot replacement.
    Add css and style loader

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,3 @@
+require('./style.css');
+
 console.log('this is a toy store');
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var WebpackNotifierPlugin = require('webpack-notifier');
 var WebpackBrowserPlugin = require('webpack-browser-plugin');
@@ -7,11 +8,24 @@ module.exports = {
         app: './src/app/app.js',
     },
 
+    output: {
+        path: path.join(__dirname, '/public/dist'),
+        publicPath: 'http://localhost:3000/',
+        filename: '[name].bundle.js',
+    },
+
     resolve: ['.', '.js'],
 
     devServer: {
         historyApiFallback: true,
         stats: 'minimal',
+        port: 3000,
+    },
+
+    module: {
+        loaders: [
+            { test: /\.css$/, loader: "style-loader!css-loader" },
+        ]
     },
 
     plugins: [
